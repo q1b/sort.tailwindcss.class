c01880550b38dc3d201a9836252e8134340748af
sacarvy <sukhpreetben10@gmail.com>
2022-01-25 14:38:32 +0530

    added variants support time is like 1ms to sort single class

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 /*
     align-baseline
     animate
@@ -168,7 +169,10 @@
 fn main() {
 	// This is my class
 	// const INITIAL_CLASS: &str = "flex absolute items-center top-0 left-0 right-9 bottom-10 justify-content m-4 p-4 px-2 border border-blue-500 shadow-xl shadow-blue-700 top-0 shrink-0 grow opacity-90 text-opacity-90 bg-opacity-50 bg-contain place-content-between bg-gradient-to-tr from-blue-500 to-sky-600 transition-opacity text-xl text-blue-900 no-underline font-bold italic translate-x-20 scale-100 rotate-90 duration-900 delay-50 ease-in-out animate-pulse not-sr-only select-none";
-    let a = "hover:text-white";
+    let a = "shadow-xl underline underline-offset-1 shadow-blue-400 bg-cover place-content-between focus:text-bold hover:text-white grid items-center justify-center hover:bg-white bg-white/90 text-xl transition-opacity duration-1000 delay-500 ease-in-out scale-200 ";
+    let start = Instant::now();
 	let result= tailwind_cli::sort_tailwindcss_classes(a);
-	println!("{}",result);
+    let duration = start.elapsed();
+	println!("The greatr result {}",result);
+    println!("Dur {:?}",duration);
 }
